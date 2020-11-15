62aae22875df6e88e32db761348f2ebba13b4e0a
Jumner Neon <69999075+Jumner@users.noreply.github.com>
2020-11-15 08:03:19 -0500

    Made a rainbow bg using a frame buffer!

--- cpp/game/src/main.cpp
+++ cpp/game/src/main.cpp
@@ -11,13 +11,18 @@
 #include "prim.hpp"
 
 //window size
-#define SCREEN_SIZE_X 640
-#define SCREEN_SIZE_Y 480
+#define WIDTH 640
+#define HEIGHT 480
 
 int main() {
   std::cout << "Starting..." << std::endl << std::endl;
 
-	SDL_Window* window = SDL_CreateWindow("Test game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_SIZE_X, SCREEN_SIZE_Y, SDL_WINDOW_OPENGL);
+	if(SDL_Init(SDL_INIT_VIDEO) != 0) { //sdl init failed
+		std::cout << "Error in sdl init: " << SDL_GetError() << std::endl;
+		return -1; 
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Test game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
 	if(!window) { //window failed
 		std::cout << "Window creation failed: " << SDL_GetError() << std::endl;
 		return -1;
@@ -31,6 +36,18 @@ int main() {
 	}
 	std::cout << "Renderer created" << std::endl;
 
+	
+
+	// SDL_Surface* surface = SDL_CreateRGBSurface(0, WIDTH, HEIGHT, 32, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
+	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	SDL_Surface* surfaceData = SDL_CreateRGBSurface(0, WIDTH, HEIGHT, 32, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
+	if(!surface) { //surface failed
+		std::cout << "Surface creation failed: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+
+	uint32_t pixelData[WIDTH*HEIGHT];
+
 	// SDL_Delay(1000); //for some reason this is needed
 
 	std::cout << "Starting game loop" << std::endl;
@@ -38,7 +55,9 @@ int main() {
 	SDL_Event quitEvent;
 	bool gameLoop = true;
 	unsigned long frameCount = 0; 
-	int v;
+	int g, b;
+	bool gMode = true;
+	bool bMode = true;
 	while(gameLoop) {
 		if(SDL_PollEvent(&quitEvent)) {
 			if(quitEvent.type==SDL_QUIT) {
@@ -46,15 +65,46 @@ int main() {
 				break;
 			}
 		}
-		v = frameCount%255;
-		SDL_SetRenderDrawColor(renderer, v, 50, 0, 255);
-		frameCount++;
-		SDL_RenderClear(renderer);
+		if(gMode) {
+			g = (int) (frameCount/3)%255;
+		} else {
+			g = 255 - (int) (frameCount/3)%255;
+		}
+		if(bMode) {
+			b = (int) (frameCount/7)%255;
+		} else {
+			b = 255 - (int)(frameCount/7)%255;
+		}
+		if(g>=255 || g<=0) {
+			gMode = !gMode;
+		}
+		if(b>=255 || b<=0) {
+			bMode = !bMode;
+		}
+			
+		
 
-		SDL_RenderPresent(renderer);
+		for(uint32_t& pixel: pixelData) {
+			pixel = 0xFF0000FF + g*65536 + b*256;
+			//make all pixels orange :)
+		}		
+
+		SDL_LockSurface(surfaceData);
+
+		surfaceData->pixels = pixelData;
+
+		SDL_UnlockSurface(surfaceData);
+
+		SDL_UpperBlit(surfaceData, nullptr, surface, nullptr);
+
+		SDL_UpdateWindowSurface(window);
+
+		frameCount++;
+
 	}
 
 	SDL_DestroyRenderer(renderer);
+	SDL_FreeSurface(surface);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
