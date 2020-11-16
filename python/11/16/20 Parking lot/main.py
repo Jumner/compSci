ead816f70f049bb226bfde2d18e0b797be8b6c03
Jumner Neon <69999075+Jumner@users.noreply.github.com>
2020-11-16 12:33:27 -0500

    Done lesson 1 handout 😃

---
+++ python/11/16/20 Parking lot/main.py
@@ -0,0 +1,45 @@
+# parking lot problem
+
+# begh were all empty
+# answer is (B) 4
+# apple packing
+
+# looseApples = 275%8
+
+# print(looseApples) #apples
+# print((275//8)%8)
+# print(((275//8)//8))
+
+# (B) 4 boxes 2 bags 3 apples
+
+# Intrusion
+
+# (B) 10:13
+
+print("Hello World")
+# print(Hello World) # Gets error "SyntaxError: invalid syntax"
+# name = input("Enter your name")
+# print(f'Hello {name}')
+# To create space after the print statement
+print("This is my first program\n\n")
+print(1,000,000)  # Prints 1 0 0
+# print(1 2) # Gets error "SyntaxError: invalid syntax"
+# print(n = 7) # Gets error "TypeError: 'n' is an invalid keyword argument for print()"
+print(7+5)
+print(f'5 is less than {7-1} which is less than 7')
+print(5, "is less that", 7-1, "which is less than", 7)
+print("\n\n")
+pi = 62.83185 / (2*10)
+print(f'The value of pi to 5 decimal places approximately {pi:.4f}')
+print(f'10/3 = {10/3:>10.2f}')
+name = 'Avery'
+print(f'--{name:^10}--')
+
+print(6 * (1 - 2 + 3))
+print(6 * 1 - (2 + 3))
+print(6 * (1 - 2) + 3)
+
+print(10/3)  # The decimal result of 10 divided by 3
+print(10//3)  # The result of 10/3 floored
+print(10 % 3)  # The remainder after 10/3
+# You can use // to get the floor and % to get the remainder
