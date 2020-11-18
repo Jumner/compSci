d84a7a706d6f5e9ac031cd8551ff972a02abace0
Jumner Neon <69999075+Jumner@users.noreply.github.com>
2020-11-18 13:44:12 -0500

    The rapid fire questions

---
+++ python/20/11/18/rapid_fire.py
@@ -0,0 +1,31 @@
+seconds = 60 * 60
+print(seconds)
+
+numSum = 8 + 9 + 10
+print(numSum)
+
+numMul = 8 * 9 * 10
+print(numMul)
+
+sec = 365.2422 * 24 * 60 * 60
+print(sec)
+
+inchMile = 12 * 5280
+print(inchMile)
+
+sqFoot = 10*12
+tile = 2*2
+tiles = sqFoot/tile
+print(tiles)
+
+ppl = 29
+shakes = 0
+ppl-=1
+while ppl > 1:
+	shakes += ppl
+	ppl -= 1
+print(shakes)
+
+nums = [16,16,16,16,6]
+avg = sum(nums)/len(nums)
+print(avg)
